5dc0656c0b639d43a30d826eb8281a77ccd4d25a
François Peyret <fpeyret@axome.com>
2019-05-20 13:59:58 +0200

    [+] FO: gestion du resize de fenetre et mouse cursor

--- js/core.js
+++ js/core.js
@@ -1,7 +1,30 @@
+/////////////////////////////////////////
+//                                     //
+//    TowerDefense ----------- 2019    //
+//                                     //
+/////////////////////////////////////////
+
+
 function setup() {
-    createCanvas(640, 480);
+    // creation d'un canvas à taille de la fenêtre
+    createCanvas(windowWidth, windowHeight);
 }
 
 function draw() {
-    ellipse(50, 50, 80, 80);
+    background(30, 30, 30);
+
+
+    // titre
+    textAlign(CENTER);
+    fill(255);
+    textSize(32);
+    text('TowerDefense', windowWidth/2, 45);
+
+    // mouse cursor
+    ellipse(mouseX, mouseY, 12, 12);
+}
+
+// gestion du resize de la fenêtre
+function windowResized() {
+    resizeCanvas(windowWidth, windowHeight);
 }
